b633ff73674d649c9e234e841006eb16b16b93e3
syahraazly <syahra.maghfiroh@ist.id>
2025-01-11 11:02:00 +0700

    feat consume data from api public

--- employment-backend/app/Providers/AppServiceProvider.php
+++ employment-backend/app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Employment;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,28 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // fetch data dari API Public
+        $response = Http::get('https://678112d285151f714b0945c6.mockapi.io/api/v1/employment/list-public-api');
+
+        if ($response->successful()) {
+            $data = $response->json();
+
+            foreach ($data as $item) {
+                // simpan data ke database
+                Employment::updateOrCreate(
+                    ['employment_no' => $item['employment_no']], 
+                    [
+                        'name' => $item['name'],
+                        'gender' => $item['gender'],
+                        'role' => $item['role'],
+                        'job_grade' => $item['job_grade'],
+                        'employment_status' => $item['employment_status'],
+                        'manager' => $item['manager'],
+                        'join_date' => $item['join_date'],
+                        'end_date' => $item['end_date'],
+                    ]
+                );
+            }
+        }
     }
 }
