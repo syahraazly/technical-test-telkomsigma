be48364add6fc94a11fefc54c6acc8c9bfb2b67a
syahraazly <syahra.maghfiroh@ist.id>
2025-01-11 10:59:35 +0700

    fix validasi model enum

--- employment-backend/app/Models/Employment.php
+++ employment-backend/app/Models/Employment.php
@@ -9,6 +9,20 @@ class Employment extends Model
 {
     use HasFactory;
 
+    // enum untuk employment_status
+    const EMPLOYMENT_STATUS_CONTRACT = 'Contract';
+    const EMPLOYMENT_STATUS_PERMANENT = 'Permanent';
+    const EMPLOYMENT_STATUS_PROBATION = 'Probation';
+
+    // enum untuk job_grade
+    const JOB_GRADE_JUNIOR = 'Junior';
+    const JOB_GRADE_MIDDLE = 'Middle';
+    const JOB_GRADE_SENIOR = 'Senior';
+
+    // enum untuk gender
+    const GENDER_MALE = 'Male';
+    const GENDER_FEMALE = 'Female';
+
     protected $fillable = [
         'name',
         'gender',
@@ -18,6 +32,24 @@ class Employment extends Model
         'employment_status',
         'manager',
         'join_date',
-        'end_date',
+        'end_date'
+    ];
+
+    // validasi enum
+    public static $employmentStatusEnum = [
+        self::EMPLOYMENT_STATUS_CONTRACT,
+        self::EMPLOYMENT_STATUS_PERMANENT,
+        self::EMPLOYMENT_STATUS_PROBATION
+    ];
+
+    public static $jobGradeEnum = [
+        self::JOB_GRADE_JUNIOR,
+        self::JOB_GRADE_MIDDLE,
+        self::JOB_GRADE_SENIOR
+    ];
+
+    public static $genderEnum = [
+        self::GENDER_MALE,
+        self::GENDER_FEMALE
     ];
 }
