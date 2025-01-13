0610de12e02ecee37cb4e2f223929f322de9dae9
syahraazly <syahra.maghfiroh@ist.id>
2025-01-13 21:17:12 +0700

    fix delete & handle cors

--- employment-backend/app/Providers/AppServiceProvider.php
+++ employment-backend/app/Providers/AppServiceProvider.php
@@ -27,26 +27,29 @@ class AppServiceProvider extends ServiceProvider
             $data = $response->json();
 
             foreach ($data as $item) {
-                // apakah data sudah ada di database lokal
-                $existingData = Employment::where('employment_no', $item['employment_no'])->first();
+                try {
+                    // apakah data sudah ada
+                    $existingData = Employment::where('employment_no', $item['employment_no'])->first();
 
-                if ($existingData && !$existingData->is_deleted) {
+                    if (!$existingData || $existingData->is_deleted) {
+                        // tambahkan data baru jika belum ada atau sudah dihapus
+                        Employment::create([
+                            'employment_no' => $item['employment_no'],
+                            'name' => $item['name'],
+                            'gender' => $item['gender'],
+                            'role' => $item['role'],
+                            'job_grade' => $item['job_grade'],
+                            'employment_status' => $item['employment_status'],
+                            'manager' => $item['manager'],
+                            'join_date' => $item['join_date'],
+                            'end_date' => $item['end_date'],
+                            'is_custom' => false, // default data dari API public
+                            'is_deleted' => false // default data tidak dihapus
+                        ]);
+                    }
+                } catch (\Exception $e) {
+                    // abaikan error duplikasi
                     continue;
-                } else {
-                    // Tambahkan data baru
-                    Employment::create([
-                        'employment_no' => $item['employment_no'],
-                        'name' => $item['name'],
-                        'gender' => $item['gender'],
-                        'role' => $item['role'],
-                        'job_grade' => $item['job_grade'],
-                        'employment_status' => $item['employment_status'],
-                        'manager' => $item['manager'],
-                        'join_date' => $item['join_date'],
-                        'end_date' => $item['end_date'],
-                        'is_custom' => false, // default data dari API public
-                        'is_deleted' => false // default data tidak dihapus
-                    ]);
                 }
             }
         }
