bf8e436e8084e703fe52d187bcb5835ed4a6e3a9
syahraazly <syahra.maghfiroh@ist.id>
2025-01-13 16:50:14 +0700

    fix conflict

--- employment-frontend/src/main.js
+++ employment-frontend/src/main.js
@@ -1,13 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import store from './store';
 import axios from 'axios';
 axios.defaults.baseURL = 'http://localhost:8000/api'
 import './assets/styles/tailwind.css';
 
 
 const app = createApp(App);
-app.use(store);
 app.mount('#app');
 app.config.globalProperties.axios = axios;
 app.config.globalProperties.$url = 'http://localhost:8000/api'
